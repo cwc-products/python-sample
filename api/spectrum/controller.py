62c316c8ef78082f2ab5ac51719ca6c70a74bb12
Christopher Cox <coxcw@eou.edu>
2020-09-06 16:38:59 -0700

    Simple cast exception and 500 handling

--- api/spectrum/controller.py
+++ api/spectrum/controller.py
@@ -1,5 +1,7 @@
-from flask import request
+import base64
+import numpy as np
 
+from flask import request
 from api import app
 
 @app.route('/spectrum', methods=['GET'])
@@ -8,11 +10,25 @@ def getSpectrum():
 
 @app.route('/spectrum', methods=['POST'])
 def postSpectrum():
-    req = request.get_json()
-    sr = float(req['sampling_rate'])
-    units = req["units"]
-    dtype = req['dtype'] if 'dtype' in req else "float64"
-    spectrum = np.frombuffer(base64.b64decode(req["spectrum"].encode('utf-8')), dtype=dtype).tolist()
-    samples_per_signal = len(spectrum)
-    duration = samples_per_signal / sr
-    return {'spectrum': spectrum}
+    try:
+        req = request.get_json()
+        try:
+            sr = float(req['sampling_rate'])
+        except:
+            #raise Exception("sample_rate", "Only floats are allowed") 
+            return {'error': 'Invalid Sampling Rate'}, 400
+        units = req["units"]
+        dtype = req['dtype'] if 'dtype' in req else "float64"
+        spectrum = np.frombuffer(base64.b64decode(req["spectrum"].encode('utf-8')), dtype=dtype).tolist()
+        samples_per_signal = len(spectrum)
+        duration = samples_per_signal / sr
+        return {'spectrum': spectrum}
+    except:
+        # Log error
+        
+        error = app.response_class(
+            response={'error': 'Internal server error'},
+            status=500,
+            mimetype='application/json'
+        )   
+        return error
---
+++ tests/test_spectrum_edge_cases.py
@@ -0,0 +1,92 @@
+
+import pytest
+import json
+import pickle 
+
+import base64
+import pandas as pd
+import numpy as np
+
+import collections 
+
+@pytest.fixture
+def spectrum_test_meta():
+    """return request meta information for all routes"""
+    mimetype = 'application/json'
+
+    return {
+        'url': '/spectrum',
+        'post': {
+            'mimetype': mimetype,
+            'headers': {
+                'Content-Type': mimetype,
+                'Accept': mimetype
+            }
+        }
+    }
+
+@pytest.fixture
+def spectrum_test_data():
+    """return test data values for all routes"""
+    # Commented out this time series as the endpoint does not 
+    # accept a proper time series.  Instead it appears to only
+    # accept a simple vector
+    # index = pd.DatetimeIndex(['2020-07-04', '2020-08-04',
+    #                      '2020-09-04', '2020-10-04'])
+    # ts = pd.Series([0, 1, 2, 3], index=index)
+    ts = np.arange(4, dtype=np.float64)
+    jsonSafe = ts #pickle.dumps(ts)
+    s = base64.b64encode(jsonSafe).decode('ascii')
+ 
+    success_data = {
+        'spectrum': s,
+        'sampling_rate': 1, 
+        'units': 1,
+        'dtype': 'float64'
+    }
+
+    broken_spectrum = {
+        'spectrum': ['a', 'b'],
+        'sampling_rate': 1, 
+        'units': 1,
+        'dtype': 'float64'
+    }
+
+    broken_sampling_rate = {
+        'spectrum': ['a', 'b'],
+        'sampling_rate': 'a', 
+        'units': 1,
+        'dtype': 'float64'
+    }
+
+    return {
+        'success_data': success_data,
+        'broken_spectrum': broken_spectrum,
+        'broken_sampling_rate': broken_sampling_rate
+    }
+
+def test_spectrum_simple_success(client, spectrum_test_meta, spectrum_test_data):
+    """Provide simple vector to generate a successful spectrum calculation"""
+    response = client.post(
+        spectrum_test_meta['url'],
+        data=json.dumps(spectrum_test_data['success_data']),
+        headers=spectrum_test_meta['post']['headers'])
+    assert collections.Counter(response.json['spectrum']) == collections.Counter(np.arange(4, dtype=np.float64)) 
+
+def test_spectrum_broken_spectrum(client, spectrum_test_meta, spectrum_test_data):
+    response = client.post(
+        spectrum_test_meta['url'],
+        data=json.dumps(spectrum_test_data['broken_spectrum']),
+        headers=spectrum_test_meta['post']['headers'])
+    assert response.mimetype == 'application/json'
+    assert response.status_code == 500
+
+def test_spectrum_broken_sampling_rate(client, spectrum_test_meta, spectrum_test_data):
+    response = client.post(
+        spectrum_test_meta['url'],
+        data=json.dumps(spectrum_test_data['broken_sampling_rate']),
+        headers=spectrum_test_meta['post']['headers'])
+    assert response.mimetype == 'application/json'
+    assert response.status_code == 400
+    print(response)
+    assert response.json['error'] == 'Invalid Sampling Rate'
